5237abd453940657a214dad2782b6f7d3a45332d
Cheng-Yu Lin <ntuaha@gmail.com>
2012-12-04 21:20:17 +0800

    神奇的參數給1

---
+++ darkmatter.m
@@ -0,0 +1,21 @@
+function y_result = darkmatter(beta,location)
+
+    c = beta(1);
+    M = beta(2);
+    e = beta(3);
+    halo_x = beta(4);
+    halo_y = beta(5);
+
+    x = location(:,1);
+    y = location(:,2);
+    
+    r = sqrt((x-halo_x).^2 + (y-halo_y).^2);
+    %r = getDistance(x,y,halo_x,halo_y);
+    phi = atan((y-halo_y)./(x-halo_x));
+    
+    l = length(phi);
+    s = l / 2;
+    y_result(1:s)  = e - M./(r(1:s)+c) .* cos(phi(1:s));
+    y_result(s+1:l)  = e - M./(r(s+1:l)+c) .* sin(phi(s+1:l));
+    
+    
--- first_code.m
+++ first_code.m
@@ -4,18 +4,45 @@ clear;close;clc;
 ansfile = '/Users/aha/Dropbox/Learning/2012_Kaggle/DarkSky/data/Training_halos.csv';
 [SkyId,numHalos,x_ref,y_ref,halo_x1,halo_y1,halo_x2,halo_y2,halo_x3,halo_y3]=textread(ansfile,'%s%d%n%n%n%n%n%n%n%n','delimiter', ',','headerlines',1);
 for i =1:300
-    
+    i
     file = ['/Users/aha/Dropbox/Learning/2012_Kaggle/DarkSky/data/Train_Skies/Training_Sky' num2str(i) '.csv' ];
     
     [id,x,y,e1,e2]=textread(file,'%s%n%n%n%n','delimiter', ',','headerlines',1);
+    clear galaxy;
+    xx = [x' x'];
+    yy = [y' y'];
+    ee = [e1' e2'];
+    galaxy(:,1) = xx;
+    galaxy(:,2) = yy;    
+    c = 1000;
+    M = 100000;
+    e = 0.22;
+    x_start = halo_x1(i)+1000;
+    y_start = halo_y1(i)-1000;
+    x_start = sum(x)/length(x);
+    y_start = sum(y)/length(y);
+    opts = statset('MaxIter',1000);
+    [beta,r,J] = nlinfit(galaxy,ee,@darkmatter,[c,M,e,x_start,y_start],opts);
     
     e = e1.^2+e2.^2;
-    %surf(x,y,e);
-    %hold on;
-    bubbleplot(x,y,e,'blue',25);    
-    hold on;
-    plot(halo_x1(i),halo_y1(i),'ro');
-    break;
-            
+    if true
+        bubbleplot(x,y,e,'blue',25);    
+        hold on;
+   
+        plot(halo_x1(i),halo_y1(i),'rs', 'MarkerEdgeColor','k','MarkerFaceColor','r','MarkerSize',10);
+        plot(beta(4),beta(5),'rs', 'MarkerEdgeColor','k','MarkerFaceColor','g','MarkerSize',10);
+        plot(x_start,y_start,'rs', 'MarkerEdgeColor','k','MarkerFaceColor','k','MarkerSize',10);
+    end
+    beta
+    dd(i) = getDistance(beta(4),beta(5),halo_x1(i),halo_y1(i))
+    dd_start(i) = getDistance(x_start,y_start,halo_x1(i),halo_y1(i))
+    %r
     
+    
+    
+    
+    if(i==1)
+        break;                
+    end
 end
+%sum(dd)/100
