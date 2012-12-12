99632c572c6bb6eb4ce27b631e8f25af13190ab3
Cheng-Yu Lin <ntuaha@gmail.com>
2012-12-12 22:26:06 +0800

    1.544

--- new_version_special_2/subfunction2.m
+++ new_version_special_2/subfunction2.m
@@ -12,7 +12,7 @@ r12 = 1;
 E2 = 0;
 
 
-step = 500;
+step = 200;
 temp_X1 =0;
 temp_Y1 =0;
 temp_X2 =0;
@@ -30,16 +30,16 @@ for XX2 = 0:step:4200
         beta0 = [c1,m1,c2,m2,e,x_start(1),y_start(1),XXX2,YYY2,c12,r1,r2,r12,E2];
         [C1 M1 C2 M2 E X1 Y1 temp_XXX2 temp_YYY2 norm] = findHalo2(index,beta0,x,y,e1,e2);
         %third
-        beta0 = [c1,m1,c2,m2,e,x_start(1),y_start(1),temp_XXX2,temp_YYY2,c12,r1,r2,r12,E2];
-        [C1 M1 C2 M2 E X1 Y1 temp_XX2 temp_YY2 norm] = findHalo2(index,beta0,x,y,e1,e2);
-        move= getDistance(temp_XX2,temp_YY2,temp_XXX2,temp_YYY2);
+%         beta0 = [c1,m1,c2,m2,e,x_start(1),y_start(1),temp_XXX2,temp_YYY2,c12,r1,r2,r12,E2];
+%         [C1 M1 C2 M2 E X1 Y1 temp_XX2 temp_YY2 norm] = findHalo2(index,beta0,x,y,e1,e2);
+        move= getDistance(temp_XXX2,temp_YYY2,XXX2,YYY2);
         if(move<temp_norm)
             %temp_length = norm;
             temp_norm = move;
             temp_X1 = X1;
-            temp_X2 = temp_XX2;
+            temp_X2 = temp_XXX2;
             temp_Y1 = Y1;
-            temp_Y2 = temp_YY2;
+            temp_Y2 = temp_YYY2;
 
         end
     end
